61d6da3fd928da1c400535377bc64c6271e21bdf
AurelienT34 <47206310+AurelienT34@users.noreply.github.com>
2020-11-10 17:18:06 +0100

    Category + Comments + Faker

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,7 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Persistence\ManagerRegistry;
+use phpDocumentor\Reflection\Types\Null_;
+use PhpParser\Node\Expr\Cast\Object_;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -55,5 +64,53 @@ class BlogController extends AbstractController
         return $this->render('blog/singleArticle.html.twig',[
             'article'=>$article
         ]);
+    }
+
+    /**
+     * @Route("/blog/creationarticle"), name="creationArticle")
+     * @Route("/blog/creationarticle/{id}/edit", name="modificationArticle")
+     * @param Article|null $article
+     * @param Request $request
+     * @param ManagerRegistry $manager
+     * @return Response
+     */
+    public function formManager(?Article $article,Request $request, ManagerRegistry  $manager): Response
+    {
+        if(!$article) {
+            $article = new Article();
+        }
+
+        if($article->getId() !== null) {
+            $article->setTitle($article->getTitle())
+                    ->setContent($article->getContent())
+                    ->setImage($article->getImage());
+        }
+
+        /**
+        $form = $this->createFormBuilder($article)
+                        ->add('title')
+                        ->add('content')
+                        ->add('image')
+                        ->getForm();
+        */
+
+        $form = $this->createForm(ArticleType::class,$article);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            if(!$article->getId()) {
+                $article->setCreateAt(new \DateTime());
+            }
+
+            $manager->getManager()->persist($article);
+            $manager->getManager()->flush();
+            return  $this->redirectToRoute('singleArticle',['id'=>$article->getId()]);
+        }
+
+        return $this->render('blog/creationArticle.html.twig',[
+            'formArticle' => $form->createView(),
+            'editMode'=> $article->getId() !== null]);
     }
 }
