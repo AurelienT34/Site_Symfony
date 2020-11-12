f4e42742770d91a761cb40f2f522a34549653971
AurelienT34 <aurelien.trouche928@gmail.com>
2020-11-12 17:27:52 +0100

    message

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -45,7 +45,7 @@ class BlogController extends AbstractController
      */
     public function home(): Response
     {
-        return $this->render('blog/index.html.twig');
+        return $this->render('Blog/index.html.twig');
     }
 
     /**
@@ -69,7 +69,7 @@ class BlogController extends AbstractController
             $manager->getManager()->flush();
             return  $this->redirectToRoute('singleArticle',['id'=>$article->getId()]);
         }
-        return $this->render('blog/singleArticle.html.twig',[
+        return $this->render('Blog/singleArticle.html.twig',[
             'article'=>$article,
             'commentForm'=>$form->createView()
         ]);
@@ -118,7 +118,7 @@ class BlogController extends AbstractController
             return  $this->redirectToRoute('singleArticle',['id'=>$article->getId()]);
         }
 
-        return $this->render('blog/creationArticle.html.twig',[
+        return $this->render('Blog/creationArticle.html.twig',[
             'formArticle' => $form->createView(),
             'editMode'=> $article->getId() !== null]);
     }
